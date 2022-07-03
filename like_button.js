279ea51cceab1ca0454f59cbe86308acb336c83e
Syrox96 <blomgren_fredrik@hotmail.com>
2022-07-03 10:13:58 +0200

    Update

--- like_button.js
+++ like_button.js
@@ -21,6 +21,24 @@ class LikeButton extends React.Component {
   }
 }
 
+function NavBarClick(element){
+ var lastActive = document.getElementById(sessionStorage.getItem("previous"));
+ lastActive.className = " ";
+
+ element.className = "active";
+
+ sessionStorage.setItem("previous",element.id);
+}
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
 const domContainer = document.querySelector('#like_button_container');
 const root = ReactDOM.createRoot(domContainer);
 root.render(e(LikeButton));
