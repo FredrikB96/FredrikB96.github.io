8a21712c60d5a125749b85217b7b8da246f81e5a
Fredrik Blomgren <fredrik_blomgren@outlook.com>
2023-10-14 22:02:58 +0900

    Update Pagelogic.js

--- build/Pagelogic.js
+++ build/Pagelogic.js
@@ -1,7 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() {
+/* document.addEventListener("DOMContentLoaded", function() {
     // Your code here
     alert("The DOM has loaded!");
-});
+}); */
 
 document.getElementById("checkButton").addEventListener("click", function() {
     const firstName = document.getElementById("firstNameDisplay").textContent.trim();
