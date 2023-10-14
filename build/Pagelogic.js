3eca53514a64da3849c7f6aa919919ae12cc823b
Fredrik Blomgren <fredrik_blomgren@outlook.com>
2023-10-14 19:45:36 +0900

    Changed to use input field

--- build/Pagelogic.js
+++ build/Pagelogic.js
@@ -1,16 +1,30 @@
-// Pagelogic.js
-document.getElementById("populateButton").addEventListener("click", function() {
-    Populate();
+document.getElementById("checkButton").addEventListener("click", function() {
+    const firstName = document.getElementById("firstNameDisplay").textContent.trim().toLowerCase();
+    const lastName = document.getElementById("lastNameInput").value.trim().toLowerCase();
+
+    if (lastName) {
+        checkEmployeeName(firstName, lastName);
+    }
 });
 
-function Populate() {
+function checkEmployeeName(firstName, lastName) {
     fetch('words.json')  // Change the URL to your JSON file or API endpoint
         .then(response => response.json())
         .then(data => {
-			//let test = JSON.parse(data);
-			const firstEmployee = data.Employees[0];
-            document.getElementById("input").textContent = firstEmployee.firstName;
-            
+            const matchingEmployee = data.Employees.find(employee => {
+                // Compare first name while ignoring case
+                return employee.firstName.toLowerCase() === firstName;
+            });
+
+            if (matchingEmployee) {
+                if (matchingEmployee.lastName.toLowerCase() === lastName) {
+                    document.getElementById("input").textContent = "✅ Last name is correct!";
+                } else {
+                    document.getElementById("input").textContent = `❌ Last name should be "${matchingEmployee.lastName}"`;
+                }
+            } else {
+                document.getElementById("input").textContent = "❌ First name not found";
+            }
         })
         .catch(error => {
             console.error('Error loading data:', error);
