13ef1efebff021d45dff22a718b4693b721c879e
Fredrik Blomgren <fredrik_blomgren@outlook.com>
2023-10-14 16:16:37 +0900

    testing

--- build/Pagelogic.js
+++ build/Pagelogic.js
@@ -6,12 +6,20 @@ function Populate() {
 }
 
 function Loadjson() {
-    return random(2);
+    return random(Math.floor(Math.random() * 3));
 }
 
 function TestNode() {
     var success = false;
 
+    const fs = require('fs');
+
+    fs.readFile('words.json', (err, data) => {
+        if (err) throw err;
+        let student = JSON.parse(data);
+        console.log(student);
+        sucess = true;
+    });
     return success;
 }
 
