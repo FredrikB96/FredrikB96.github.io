0633df6966ac93907da5f566019835259d0f8742
Fredrik Blomgren <fredrik_blomgren@outlook.com>
2023-10-14 21:36:12 +0900

    Updated input fields

--- build/Pagelogic.js
+++ build/Pagelogic.js
@@ -1,32 +1,48 @@
+window.onload = function() {
+    // Your code here
+    alert("The page has loaded!");
+};
+
 document.getElementById("checkButton").addEventListener("click", function() {
-    const firstName = document.getElementById("firstNameDisplay").textContent.trim().toLowerCase();
-    const lastName = document.getElementById("lastNameInput").value.trim().toLowerCase();
+    const firstName = document.getElementById("firstNameDisplay").textContent.trim();
+    const lastNameInput = document.getElementById("lastNameInput");
+    const lastName = lastNameInput.value.trim();
 
     if (lastName) {
-        checkEmployeeName(firstName, lastName);
+        checkEmployeeName(firstName, lastName, lastNameInput);
     }
 });
 
-function checkEmployeeName(firstName, lastName) {
+function checkEmployeeName(firstName, lastName, lastNameInput) {
     fetch('words.json')  // Change the URL to your JSON file or API endpoint
         .then(response => response.json())
         .then(data => {
             const matchingEmployee = data.Employees.find(employee => {
                 // Compare first name while ignoring case
-                return employee.firstName.toLowerCase() === firstName;
+                return employee.firstName.toLowerCase() === firstName.toLowerCase();
             });
 
             if (matchingEmployee) {
-                if (matchingEmployee.lastName.toLowerCase() === lastName) {
-                    document.getElementById("input").textContent = "✅ Last name is correct!";
-                } else {
-                    document.getElementById("input").textContent = `❌ Last name should be "${matchingEmployee.lastName}"`;
-                }
-            } else {
-                document.getElementById("input").textContent = "❌ First name not found";
+                const correctLastName = matchingEmployee.lastName;
+                highlightLastName(lastNameInput, correctLastName);
             }
         })
         .catch(error => {
             console.error('Error loading data:', error);
         });
+}
+
+function highlightLastName(inputElement, correctLastName) {
+    const lastName = inputElement.value;
+    let resultHtml = '';
+
+    for (let i = 0; i < lastName.length; i++) {
+        if (lastName[i] === correctLastName[i]) {
+            resultHtml += `<span class="highlight-correct">${lastName[i]}</span>`;
+        } else {
+            resultHtml += `<span class="highlight-incorrect">${lastName[i]}</span>`;
+        }
+    }
+
+    inputElement.innerHTML = resultHtml;
 }
