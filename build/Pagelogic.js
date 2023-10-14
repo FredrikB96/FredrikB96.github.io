6308eb2745d37a176481d7b69f4c9fdb87523fff
Fredrik Blomgren <fredrik_blomgren@outlook.com>
2023-10-14 17:38:19 +0900

    testing chatgpt solution

--- build/Pagelogic.js
+++ build/Pagelogic.js
@@ -1,15 +1,16 @@
+// Pagelogic.js
+document.getElementById("populateButton").addEventListener("click", function() {
+    Populate();
+});
+
 function Populate() {
-    var object = Loadjson();
-    document.getElementById("input").innerHTML = object;
-    alert("testing");
+    fetch('words.json')  // Change the URL to your JSON file or API endpoint
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("input").textContent = JSON.stringify(data);
+            alert("Data loaded successfully");
+        })
+        .catch(error => {
+            console.error('Error loading data:', error);
+        });
 }
-
-function Loadjson() {
-const fs = require('fs');
-
-let rawdata = fs.readFileSync('words.json');
-let student = JSON.parse(rawdata);
-console.log(student);
-return "peter";
-}
-
