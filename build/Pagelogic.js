7a0f0e5ca148801d6abc4de110f333ead72b61fb
Fredrik Blomgren <fredrik_blomgren@outlook.com>
2023-10-14 17:46:33 +0900

    Update Pagelogic.js

--- build/Pagelogic.js
+++ build/Pagelogic.js
@@ -7,8 +7,9 @@ function Populate() {
     fetch('words.json')  // Change the URL to your JSON file or API endpoint
         .then(response => response.json())
         .then(data => {
-            document.getElementById("input").textContent = JSON.stringify(data);
-            alert("Data loaded successfully");
+			var test = JSON.parse(data).Employees[1].firstName;
+            document.getElementById("input").textContent = test;
+            
         })
         .catch(error => {
             console.error('Error loading data:', error);
